6bb304960db0367a71daa701340d9111fad5bbef
Noah Wager <noah.wager@canonical.com>
2024-09-16 16:23:31 -0700

    Don't force SAUCE notes to be at beginning of subject
    Allow "SAUCE" and other "UBUNTU" notes to be anywhere in the subject,
    not just the beginning. The main example is a reverted sauce patch,
    where the subject is 'Revert "UBUNTU: SAUCE: ..."'.
    
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- patchbot.py
+++ patchbot.py
@@ -97,10 +97,10 @@ class CommitChecker:
 
     def check_provenance(self, commit: GitCommit) -> bool:
         message_lines = commit.message.splitlines()
-        if re.compile(r'^(NVIDIA|UBUNTU): SAUCE:').match(commit.subject):
+        if re.compile(r'(NVIDIA|UBUNTU): SAUCE:').search(commit.subject):
             return True # sauce patch, no upstream provenance
 
-        if re.compile(r'^UBUNTU:').match(commit.subject):
+        if re.compile(r'UBUNTU:').search(commit.subject):
             return True # other Ubuntu-specific patch, no upstream provenance
 
         line_match = match_and_idx(message_lines, re.compile(r'(cherry.?pick|back.?port).*\s([a-z0-9]+)\)?$'))
